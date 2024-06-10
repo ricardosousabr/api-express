489db446c28b60496b393719fcebead4104429b6
Ricardo <ricardo.br.pi@gmail.com>
2024-06-10 09:20:46 -0300

    builds: add variable

--- src/service.js
+++ src/service.js
@@ -1,12 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express();
-const port = 3000;
-console.log("Hello world!");
 
 app.get('/', () => {
   console.log("Rodando")
 })
 
-app.listen(port, () => {
-  console.log("Rodando")
+app.listen(process.env.PORT, () => {
+  console.log(`Rodando na porta ${process.env.PORT}`)
 })
